37aeaec9184cb8c0acdd4508c0af3ad4ccfcb1e7
Albalad <alvaro.albalad@gmail.com>
2024-10-20 21:05:47 +0000

    boton

---
+++ src/js/component/Boton.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+
+
+const Boton = (props) => {
+    return (
+        <button type="button" class="btn btn-primary my-2">Find Out More!</button>
+    )
+};
+
+export default Boton;
