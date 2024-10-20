7be2c30e93b92b1816eb2c46090020a782239e4a
Albalad <alvaro.albalad@gmail.com>
2024-10-20 21:47:55 +0000

    footer

---
+++ src/js/component/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = (props) => {
+    return (
+        
+            <footer className='bg-dark piePagina'>
+                Copyright@ Yor Website 2024
+            </footer>
+        
+    )
+};
+
+export default Footer;
